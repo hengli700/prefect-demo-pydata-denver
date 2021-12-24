90b1a49e15e29d257213d874fa91255d7cd7636d
heng-dev <heng-dev@Hengs-MacBook-Pro.local>
2021-12-23 16:16:47 -0800

    starting scripts for workflow in prefect

---
+++ demo.py
@@ -0,0 +1,20 @@
+from prefect import task, Flow
+
+
+@task
+def hello_world():
+    print("HELLO WORLD!")
+    return 'Hello Prefect!'
+
+
+@task
+def prefect_say(s: str):
+    print(s)
+
+
+with Flow('my first flow') as f:
+    r = hello_world()
+    s2 = prefect_say(r)
+
+
+f.run()
